e5343480229abd31e655daf2d5c9ce695492d120
VictorVeelazquez <93061751+VictorVeelazquez@users.noreply.github.com>
2022-07-26 18:56:51 -0500

    main

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"os"
+)
+
+func main() {
+	key, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Genera la clave privada
+	pkcs1PrivateKey := x509.MarshalPKCS1PrivateKey(key)
+	block := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: pkcs1PrivateKey,
+	}
+	// Escribe en el archivo
+	file, err := os.Create("private.pem")
+	pem.Encode(file, block)
+
+	// Genera la clave pública
+	PublicKey := &key.PublicKey
+	// La clave pública se genera a partir de la clave privada
+	pkixPublicKey, err := x509.MarshalPKIXPublicKey(PublicKey)
+	block1 := &pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: pkixPublicKey,
+	}
+	file2, err := os.Create("public.pem")
+	encode := pem.Encode(file2, block1)
+	fmt.Println(encode)
+
+}
